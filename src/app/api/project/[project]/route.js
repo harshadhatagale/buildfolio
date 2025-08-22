28f4fa7e8cc9a571fdad872d7bfa36e4308fa2b2
harshadhatagale <harshadhatagale6@gmail.com>
2025-08-22 18:54:39 +0530

    Hello

--- src/app/api/project/[project]/route.js
+++ src/app/api/project/[project]/route.js
@@ -2,69 +2,68 @@ import dbConnect from "@/lib/db"
 import Project from "../../../../../models/Project"
 import { NextResponse } from "next/server"
 
-export async function GET(req, {params}){
-    const {project}= await params
-    try
-    {
+export async function GET(req, { params }) {
+    const { project } = await params
+    try {
         await dbConnect()
-        const myproject= await Project.findById(project)
-        if(!myproject)
-        {
-            return NextResponse.json({error: "Project not found !"}, {status: 404})
+        const myproject = await Project.findById(project)
+        if (!myproject) {
+            return NextResponse.json({ error: "Project not found !" }, { status: 404 })
         }
-        return NextResponse.json({myproject}, {status: 200})
+        return NextResponse.json({ myproject }, { status: 200 })
     }
-    catch(error)
-    {
-        return NextResponse.json({error: "Failed to fetch project !"}, {status: 501})
+    catch (error) {
+        return NextResponse.json({ error: "Failed to fetch project !" }, { status: 501 })
     }
 }
 
-export async function DELETE(req, {params}){
-    const {project}= await params
-    try
-    {
+export async function DELETE(req, { params }) {
+    const { project } = await params
+    try {
         await dbConnect()
-        const deletedProject= await Project.findByIdAndDelete(project)
-        if(!deletedProject)
-        {
-            return NextResponse.json({error: "Project not found !"}, {status: 404})
+        const deletedProject = await Project.findByIdAndDelete(project)
+        if (!deletedProject) {
+            return NextResponse.json({ error: "Project not found !" }, { status: 404 })
         }
-        return NextResponse.json({message: "Project deleted succesfully !"}, {status: 200})
+        return NextResponse.json({ message: "Project deleted succesfully !" }, { status: 200 })
     }
-    catch(error)
-    {
-        return NextResponse.json({error: "Failed to delete project !"}, {status: 501})
+    catch (error) {
+        return NextResponse.json({ error: "Failed to delete project !" }, { status: 501 })
     }
 }
 
 export async function PATCH(req, { params }) {
-    const { project } =await params // Removed unnecessary await
+    const { project } = await params // Removed unnecessary await
     const body = await req.json()
     try {
         await dbConnect()
         const updatedProject = await Project.findByIdAndUpdate(
-            project, 
-            body, 
+            project,
+            body,
             { new: true }
         )
         if (!updatedProject) {
             return NextResponse.json(
-                { error: "Project not found!" }, 
+                { error: "Project not found!" },
                 { status: 404 }
             )
         }
         return NextResponse.json(
-            { 
+            {
                 message: "Project updated successfully!",
-                project: updatedProject 
-            }, 
+                project: updatedProject
+            },
             { status: 200 }
         )
     } catch (error) {
         return NextResponse.json(
-            { error: "Failed to update project!"+ error }, 
-            { status: 500 } // Changed from 501 to 500
+            { error: "Failed to update project!" + error },
+            { status: 500 },
+            {
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                }
+            }
         )
     }
 }
--- src/components/sections/SimpleSection.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SimpleSection() {
-  return (
-    <div>SimpleSection</div>
-  )
-}
