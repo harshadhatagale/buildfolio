713985d062faa60edea9d7abb27aa631821c641f
Harshad Hatagale <harshadhatagale6@gmail.com>
2026-01-01 15:27:21 +0530

    Release ver -1

--- src/components/dashboard/editor/ShareProject.jsx
+++ src/components/dashboard/editor/ShareProject.jsx
@@ -13,7 +13,7 @@ import { slugify } from "./SectionProperties/NavProps"
 export default function ShareProject() {
     const [copied, setCopied] = useState(false)
     const project= useSelector((state)=> state.portfolio.project)
-    const portfolioLink = `https://buildfolio-pro.vercel.app/${project.urlSlug}`// replace with dynamic link
+    const portfolioLink = `${process.env.NEXT_PUBLIC_SITE_URL}/${project.urlSlug}`// replace with dynamic link
 
     const handleCopy = () => {
         navigator.clipboard.writeText(portfolioLink)
