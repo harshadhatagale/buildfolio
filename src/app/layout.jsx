cd70e11504196410115d5f1bed27e519c65da9a9
Harshad Hatagale <79795999+harshadhatagale@users.noreply.github.com>
2026-01-01 11:23:14 +0530

    Import Script component from next/script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/theme-provider";
+import Script from "next/script";
 import { Outfit, Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import NextTopLoader from "nextjs-toploader";
