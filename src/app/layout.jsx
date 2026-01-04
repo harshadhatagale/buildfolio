fa1a9f9d4ee1d1114183e9f59ddea2a24b1a181d
Harshad Hatagale <harshadhatagale6@gmail.com>
2026-01-04 13:20:57 +0530

    Vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -64,6 +64,7 @@ export default function RootLayout({ children }) {
               showAtBottom={false}
             />
             {children}
+            <Analytics/>
           </ThemeProvider>
         </body>
       </html>
