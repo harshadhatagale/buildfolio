eeff8343c6114642e9b7820bc13f3258fb64b054
harshadhatagale <harshadhatagale6@gmail.com>
2026-01-17 10:16:04 +0530

    middleware issue

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,22 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-const isProtectedRoute = createRouteMatcher(['/profile', '/dashboard(.*)'])
-const isPublicRoute = createRouteMatcher(['/', '/home', '/about', "/(.*)"])
-export default clerkMiddleware(async (auth, req) => {
-  const userAgent = req.headers.get("user-agent") || ""
-  if (userAgent.includes("Googlebot")) {
-    return NextResponse.next()
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/profile",
+  "/api/(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth().protect();
   }
-  if (isProtectedRoute(req)) await auth.protect()
-  if (isPublicRoute(req)) {
-    return NextResponse.next()
-  }
-  return NextResponse.next()
+
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/dashboard(.*)",
+    "/profile",
+    "/api/(.*)",
   ],
 };
